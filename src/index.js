85abc12e794dde67b18c78d0e28ee9e3d763fb96
defauth98 <neto.daniribeiro@gmail.com>
2020-05-17 13:14:37 -0300

    Mudança na estrutura de arquivos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const sbd = require("sbd");
-const { joinSetences } = require("./src/joinSetences");
-const { getText } = require("./src/getText.js");
+const { joinSetences } = require("./utils/joinSetences.js");
+const { getText } = require("./utils/getText.js");
 
-const englishText = getText("./texts/textEnglish.txt");
-const portugueseText = getText("./texts/textPortuguese.txt");
+const englishText = getText(`${__dirname}/texts/textEnglish.txt`);
+const portugueseText = getText(`${__dirname}/texts/textPortuguese.txt`);
 
 function getOnlySetences(textArray) {
   const text = textArray.reduce((previus, current) => `${previus}${current}`);
@@ -14,11 +14,11 @@ function getOnlySetences(textArray) {
 const englishSetences = getOnlySetences(englishText);
 const portugueseSetences = getOnlySetences(portugueseText);
 
-console.log("Texto em ingles:")
-console.log(englishSetences.toString())
+console.log("Texto em ingles:");
+console.log(englishSetences.toString());
 
-console.log('\n\n\n')
-console.log("Frase linha a linha")
+console.log("\n\n\n");
+console.log("Frase linha a linha");
 if (englishSetences.length === portugueseSetences.length) {
   joinSetences(englishSetences, portugueseSetences);
 } else {
